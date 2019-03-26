e91f01fdb9d0741e113ad9e54b62eb300036da7f
Kelsey Low <kklowlow@gmail.com>
2019-03-26 03:47:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 1, 1);
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
+INSERT INTO series (title, author_id, sub_genre_id) VALUES ("Lord of the Rings", 1, 1);
+INSERT INTO series (title, author_id, sub_genre_id) VALUES ("Harry Potter", 2, 2);
 
 INSERT INTO subgenres (name) VALUES ("Fantasy");
 INSERT INTO subgenres (name) VALUES ("Magic");

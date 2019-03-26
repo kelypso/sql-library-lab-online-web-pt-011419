7ca6ca009eb31f75710fe17d369b9cfdf58da895
Kelsey Low <kklowlow@gmail.com>
2019-03-26 03:55:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name AS author, sub_genre.name AS subgenre 
+  "SELECT authors.name AS author, sub_genre.name AS sub_genre 
   
   "
 end

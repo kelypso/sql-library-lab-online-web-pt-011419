fe65c4f8201e66b00d88dce628692c270426bc10
Kelsey Low <kklowlow@gmail.com>
2019-03-26 04:04:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,6 +41,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id) AS books FROM character_books 
-    
+    INNER JOIN characters
+      ON 
   "
 end

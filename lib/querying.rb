a49c14659acfdddb31ade6c4c13071398c13e7dd
Kelsey Low <kklowlow@gmail.com>
2019-03-26 03:52:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,8 +7,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT character.name, character.motto FROM characters
-  "
+  "SELECT name, motto FROM characters
+    ORDER BY length(motto) DESC
+    LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
